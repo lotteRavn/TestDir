a9380cb2e886c1959fb015abd49569e9ef503f14
Lotte <lotteravn@live.dk>
2020-09-01 13:50:25 +0200

    added html, changed js

---
+++ cf1.js
@@ -0,0 +1,79 @@
+// c++ kommentar af Lotte Ravn
+'use strict';
+
+let tails = 0;
+let heads = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+console.log('heads: ' + heads);
+console.log('tails: ' + tails);
